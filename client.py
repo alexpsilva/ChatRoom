b3531a55bdbf0ca7961b7581ab56c7062c1f0d5d
AlexPS <alex.paivasilva@poli.ufrj.br>
2017-06-14 20:07:55 -0300

    Implemented a better version of both the server and client

--- client.py
+++ client.py
@@ -1,13 +1,14 @@
-import socket, select, string, sys
- 
-def prompt() :
-	sys.stdout.write('<You> ')
-	sys.stdout.flush()
+import socket, select, string, sys, queue
 
-class ChatClient():
+from threading import *
+from tkinter import *
+
+class ChatClient(Thread):
 	def __init__(self, host, port):
+		Thread.__init__(self)
 		self.host = host
 		self.port = int(port)
+		self.output_queue = queue.Queue()
 
 		try:
 			self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -16,23 +17,25 @@ class ChatClient():
 			#(to-do) evaluate if it is correct to 'sys.exit()'
 			sys.exit()
 
+	def sendMsg(self, msg):
+		data = bytes(msg, 'UTF-8')
+		self.client_socket.send(data)
+
 	def run(self):
 		try :
 			self.client_socket.connect((self.host, self.port))
 		except socket.error:
 			print('Unable to connect to (' + str(self.host) + ',' + str(self.port) + ')')
-			#(to-do) evaluate if it is correct to 'sys.exit()'
-			sys.exit()
+			return
 
 		print('Connection successfull')
-		prompt();
 
 		#(to-do) evaluate if there is no better condition other than 'while 1'
 		while 1:
 			socket_list = [sys.stdin, self.client_socket]
 			
 			# Get the list sockets which are readable
-			read_sockets, write_sockets, error_sockets = select.select(socket_list , [], [])
+			read_sockets, write_sockets, error_sockets = select.select(socket_list , [self.client_socket], [])
 			
 			for sock in read_sockets:
 
@@ -42,30 +45,46 @@ class ChatClient():
 						data = sock.recv(4096).decode('UTF-8')
 					except socket.error:
 						print('Could not reach server')
-						sys.exit()
+						sock.close()
+						return
 
 					if not data :
 						# Received a blank message, therefore, the server is down
-						print('\nDisconnected from server')
-						#(to-do) evaluate if it is correct to 'sys.exit()'
-						sys.exit()
+						print('Disconnected from server')
+						sock.close()
+						return
 					else :
 						# Valid message received
-						sys.stdout.write(data)
-						prompt()
-				 
+
+						# (to-do) Add msg to chat window here
+
+						sys.stdout.write(data)				 
+						sys.stdout.flush()
 				else:
-					# Receive send input from the user
-					data = bytes(sys.stdin.readline(), 'UTF-8')
-					self.client_socket.send(data)
-					prompt()
+					# Send input from the user
+					msg = sys.stdin.readline()
+
+					if msg == '\n' :
+						#(to-do) remove this and make a better logic for closing a connection
+						sock.close()
+						return
+					else:
+						self.output_queue.put(msg)
+
+			for sock in write_sockets:
+				while not self.output_queue.empty():
+					msg = self.output_queue.get_nowait()
+					sock.send(bytes(msg, 'UTF-8'))
 
 #End of 'ChatClient' definition
 
+def main():
+	if(len(sys.argv) !=	 3) :
+		print('Invalid number of arguments')
+		sys.exit()
 
-if(len(sys.argv) !=	 3) :
-	print('Invalid number of arguments')
-	sys.exit()
+	client = ChatClient(sys.argv[1], sys.argv[2])
+	client.run()
 
-client = ChatClient(sys.argv[1], sys.argv[2])
-client.run()
+if __name__ == "__main__":
+	main()
--- server.py
+++ server.py
@@ -1,10 +1,9 @@
-import socket, select, sys
+import socket, select, sys, queue
 
 class ChatServer():
 	def __init__(self, port):
 		self.port = port
 		self.buffer_size = 4096
-		self.connection_list = []
 
 		try:
 			self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -14,52 +13,83 @@ class ChatServer():
 
 		#(to-do) unsure about these configurations
 		self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-		self.server_socket.bind(('0.0.0.0', port))
+		self.server_socket.bind(('localhost', port))
 		self.server_socket.listen(10)
 
-		self.connection_list.append(self.server_socket)
+		self.inputs = [self.server_socket]
+		self.outputs = []
+
+		self.message_queues = {}
+
 		print('Chat server started on port ' + str(port))
 
 	def broadcast(self, msg):
-		for send_socket in self.connection_list:
+		#(to-do) remove this print
+		print(msg)
+		for send_socket in self.inputs:
 			if send_socket is not self.server_socket:
-				try:
-					send_socket.send(bytes(msg, 'UTF-8'))
-				except socket.error:
-					send_socket.close()
-					self.connection_list.remove(send_socket)
+				self.message_queues[send_socket].put(msg)
+
+				if send_socket not in self.outputs:
+					self.outputs.append(send_socket)
+
+	def remove(self, sock):
+		if sock in self.outputs:
+			self.outputs.remove(sock)
+		self.inputs.remove(sock)
+		sock.close()
+
+		del self.message_queues[sock]
 
 	def run(self):
 		while 1:
 			# Get the list sockets which are ready to be read through select
-			read_sockets,write_sockets,error_sockets = select.select(self.connection_list,[],[])
-	 
+			read_sockets,write_sockets,error_sockets = select.select(self.inputs, self.outputs, self.inputs)
+
 			for sock in read_sockets:
 				if sock is self.server_socket:
 					# New Connection
-					new_socket, addr = self.server_socket.accept()
+					new_socket, new_addr = self.server_socket.accept()
 					new_socket.setblocking(0)
-					self.connection_list.append(new_socket)
-					self.broadcast('(%s, %s) entered the room\n' % addr)			 
+
+					self.inputs.append(new_socket)
+					self.message_queues[new_socket] = queue.Queue()
+
+					self.broadcast('<(%s, %s)> entered the room' % new_addr)			 
 				else:
 					# Message sent from a client
 					try:
-						data = sock.recv(self.buffer_size).decode('UTF-8')
-						if data:
-							self.broadcast('\r' + '<' + str(sock.getpeername()) + '> ' + data)
-						else:
-							sock.close()
-							self.connection_list.remove(sock)
-							self.broadcast('(%s, %s) has left the room\n' % addr)
-					 
+						data = sock.recv(self.buffer_size).decode('UTF-8')					 
 					except socket.error:
-						sock.close()
-						self.connection_list.remove(sock)
-						self.broadcast('(%s, %s) has left the room\n' % addr)
+						sock_addr = str(sock.getpeername())
+						self.remove(sock)
+						self.broadcast('<' + sock_addr + '> ' + 'has left the room')
+					else:
+						if data:
+							# Valid message received from the client
+							self.broadcast('<' + str(sock.getpeername()) + '> ' + data)
+						else:
+							# Received a blank message, therefore, the client has closed the socket
+							sock_addr = str(sock.getpeername())
+							self.remove(sock)
+							self.broadcast('<' + sock_addr + '> ' + 'has left the room')
 
-		self.server_socket.close()
+			for sock in write_sockets:
+				while not self.message_queues[sock].empty():
+					msg = self.message_queues[sock].get_nowait()
+					sock.send(bytes(msg, 'UTF-8'))
+				self.outputs.remove(sock)
+
+			for sock in error_sockets:
+				sock_addr = str(sock.getpeername())
+				self.remove(sock)
+				self.broadcast('<' + sock_addr + '> ' + 'has left the room')
 
 #End of 'ChatServer' definition
 
-server = ChatServer(5000)
-server.run()
+def main():
+	server = ChatServer(5000)
+	server.run()
+
+if __name__ == "__main__":
+	main()
